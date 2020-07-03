9f5bd6fce315a9f7b79233e36217e0fa9786990d
Jose Lopez <30425866+JDByte01@users.noreply.github.com>
2020-07-03 00:07:04 -0600

    Front-End

--- routes/web.js
+++ routes/web.js
@@ -3,15 +3,30 @@ const Route = express.Router();
 
 //Rutas
 Route.get('/', (req, res) =>{
-	res.render('index',{title:'Inicio | CPy'});
+	res.render('index',{title:'Inicio | CPy',navItem:'home'});
 });
 
 Route.get('/about', (req, res) => {
-	res.render('about',{title:'Acerca de | CPy'});
+	res.render('about',{title:'Acerca de | CPy',navItem:'about'});
 });
 
 Route.get('/translate', (req, res) => {
-	res.render('traductor',{title:'Traductor | CPy'});
+	res.render('traductor',{title:'Traductor | CPy',navItem:'translate'});
 });
 
 module.exports = Route;
+
+//Comentario
+
+/*
+app.route('/book')
+  .get(function(req, res) {
+    res.send('Get a random book');
+  })
+  .post(function(req, res) {
+    res.send('Add a book');
+  })
+  .put(function(req, res) {
+    res.send('Update the book');
+  });
+*/
